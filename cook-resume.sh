3ed8bebc26b24a50a4ab054af91d1cd5b9bf6af6
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2025-01-27 22:20:08 +0500

    Define a shell script to build an image, run a container, copy a resume from the container

---
+++ cook-resume.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+if [ ! -f "${1}" ]; then
+  echo "Specified file doesn't exist."
+  exit 0
+fi
+
+resume="$(echo "${1}" | rev | cut --delimiter='/' --fields=1 | rev)"
+
+docker build --tag cook-resume --build-arg RESUME="${resume}" "$(pwd)/"
+
+if [ "$(docker container ls --all --quiet --filter 'name=cook-resume' | wc --lines)" -ge 1 ]; then
+  docker rm cook-resume
+fi
+
+resume=$(basename --suffix='.md' "${resume}").pdf
+
+docker run --name=cook-resume --interactive --tty cook-resume && \
+docker cp "cook-resume:/home/cook-resume/${resume}" "./"
