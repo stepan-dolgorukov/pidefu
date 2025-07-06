b4b95211640a68b01e81284cdd50f65d5dd7cf82
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2025-07-06 13:03:36 +0500

    Define an algorithm to determine whether a file is regular

--- pidefu.go
+++ pidefu.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("A name of file wasn't specified.")
+		os.Exit(0)
+	}
+
+	var nameOfSourceFile = os.Args[1]
+	description, pointerToError := os.Stat(nameOfSourceFile)
+
+	if pointerToError != nil {
+		fmt.Println("Unable to get a description about the file with the specified name.")
+		os.Exit(0)
+	}
+
+	if os.IsNotExist(pointerToError) {
+		fmt.Println("A file with specified name doesn't exist.")
+		os.Exit(0)
+	}
+
+	if !description.Mode().IsRegular() {
+		fmt.Println("Specified file isn't regular.")
+		os.Exit(0)
+	}
 }
