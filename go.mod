3d35925f192ccf9a5d15ffb92d50ede4ead4ad63
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2025-07-10 22:18:47 +0500

    Define a type of the file content

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/stepan-dolgorukov/pidefu
+
+go 1.24.4
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.9 // direct
+	golang.org/x/net v0.39.0 // indirect
+)
--- pidefu.go
+++ pidefu.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
 func main() {
@@ -12,20 +15,36 @@ func main() {
 	}
 
 	var nameOfSourceFile = os.Args[1]
-	description, pointerToError := os.Stat(nameOfSourceFile)
+	description, errorStat := os.Stat(nameOfSourceFile)
 
-	if pointerToError != nil {
+	if errorStat != nil {
 		fmt.Println("Unable to get a description about the file with the specified name.")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
-	if os.IsNotExist(pointerToError) {
+	if os.IsNotExist(errorStat) {
 		fmt.Println("A file with specified name doesn't exist.")
-		os.Exit(0)
+		os.Exit(2)
 	}
 
 	if !description.Mode().IsRegular() {
 		fmt.Println("Specified file isn't regular.")
-		os.Exit(0)
+		os.Exit(3)
 	}
+
+	pointerToTypeOfContent, errorDetect := mimetype.DetectFile(nameOfSourceFile)
+
+	if errorDetect != nil {
+		fmt.Println("Fail to detect a type of the file content.")
+		os.Exit(4)
+	}
+
+	typeOfContent := pointerToTypeOfContent.String()
+
+	if !strings.HasPrefix(typeOfContent, "text/plain") {
+		fmt.Println("Unsupported file content type")
+		os.Exit(5)
+	}
+
+	fmt.Println(typeOfContent)
 }
